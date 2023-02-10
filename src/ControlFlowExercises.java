c5cbe7039004b196142d7e26c5fc46236053d4d1
Leonard Karoly <leonard.a.karoly@gmail.com>
2023-02-10 16:28:01 -0600

    Hangman game

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -89,15 +89,7 @@ public class ControlFlowExercises {
 //        int userNum = Integer.parseInt(scanner.next());
 //        System.out.println("\nHere is your table!\n\nnumber | squared | cubed \n------ | ------- | -----");
 //        for (int i = 1; i <= userNum; i++) {
-//            int sqNum = i * i;
-//            int cuNum = i * i * i;
-//            if ( sqNum < 10) {
-//                System.out.printf("%d      | %d       | %d\n", i, sqNum, cuNum);
-//            } else if ( sqNum < 100) {
-//                System.out.printf("%d      | %d      | %d\n", i, sqNum, cuNum);
-//            } else {
-//                System.out.printf("%d     | %d     | %d\n", i, sqNum, cuNum);
-//            }
+//            System.out.printf("%-6d | %-7d | %-5d%n", i, i * i, i * i * i);
 //        }
         /** #4
          * Convert given number grades into letter grades.
@@ -115,46 +107,128 @@ public class ControlFlowExercises {
          * D : 66 - 60
          * F : 59 - 0
          */
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Let's convert a number grade into a Letter Grade!\nWhat number grade would you like to convert? ");
-        int userNum = Integer.parseInt(scanner.next());
-
-        if(userNum > 100 || userNum < 0) {
-            System.out.println(userNum + " is an invalid grade.");
-        } else if (userNum >= 88 ) {
-
-            if (userNum > 94) {
-                System.out.println("A+");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Let's convert a number grade into a Letter Grade!\nWhat number grade would you like to convert? ");
+//        int userNum = Integer.parseInt(scanner.next());
+//
+//        if(userNum > 100 || userNum < 0) {
+//            System.out.println(userNum + " is an invalid grade.");
+//        } else if (userNum >= 88 ) {
+//
+//            if (userNum > 94) {
+//                System.out.println("A+");
+//            } else {
+//                System.out.println("A-");
+//            }
+//
+//        } else if (userNum >= 80) {
+//
+//            if (userNum > 84) {
+//                System.out.println("B+");
+//            } else {
+//                System.out.println("B-");
+//            }
+//
+//        } else if ( userNum >= 67 ){
+//
+//            if (userNum > 74) {
+//                System.out.println("C+");
+//            } else {
+//                System.out.println("C-");
+//            }
+//
+//        } else if ( userNum >= 60 ){
+//
+//            if (userNum > 64) {
+//                System.out.println("D+");
+//            } else {
+//                System.out.println("D-");
+//            }
+//
+//        } else {
+//            System.out.println('F');
+//        }
+        /** Bonus #1
+         * # Java Control Structures and Loops Bonuses  ### Simple Chat Bot
+         *  - Be creative in communicating with your -
+         *  user different options and for given scenerios. Create at three phases of the chat, for example...
+         * 			                 "How are you?" (AI says)
+         * 			           |                              |
+         * 			         good                        bad (user types)
+         *                  ------                      -----------------
+         * 	   "Awesome! Are you super happy?" 	    "Will cake make you better?"
+         *                    |                                    |
+         * 	          yes          no                     yes             no
+         * Keep the chat options relatively simple and plan it out clearly before coding it.    */
+//            System.out.println("How are you?   ( good/bad )");
+//            Scanner scanner = new Scanner(System.in);
+//            String userType = scanner.nextLine();
+//            if (userType.equalsIgnoreCase("good")) {
+//                System.out.println("Awesome! Are you super happy?");
+//                userType = scanner.nextLine();
+//                String response = userType.equalsIgnoreCase("yes") ? "Yay" : "aww";
+//                System.out.println(response);
+//            } else if (userType.equalsIgnoreCase("bad")) {
+//                System.out.println("Will cake make you better?");
+//                userType = scanner.nextLine();
+//                String response = userType.equalsIgnoreCase("yes") ? "Awesome! Let's get some cake :)" : "Look at you being all healthy...";
+//                System.out.println(response);
+//            }
+        /** Bonus #2
+         * ### Word Guessing Game
+         * - prompt a player to enter a word
+         * - use a loop to print out several empty lines to hide the answer
+         * - allow the user to guess the letters in the word
+         * - track the number of letter guesses
+         * - display to the user the partially completed word for each correctly guessed character
+         * - before prompting the user to guess a letter, give them the option to solve the word
+         * - limit the number of guesses based on the length of the word for an extra challenge!
+         */
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Let's play a game! \nInput a random word : ");
+        String word = sc.nextLine();
+        int wordLength = word.length(), maxGuesses = wordLength + 3, guesses = 0;
+        char[] hiddenWord = new char[wordLength];
+        for( int i = 0; i< wordLength; i++ ) {
+            hiddenWord[i] = '_';
+        }
+        while ( guesses < maxGuesses ) {
+            System.out.print("""
+                    \n\n\n\n\n\n\n\n\n\n\n\n\n\n
+                                H A N G M A N 
+                                _________
+                                |       |
+                                        |
+                                        |
+                                        |
+                                     -------        by: Leo
+                    """);
+            System.out.println(new String(hiddenWord) + "\nGuesses left: " + (maxGuesses - guesses));
+            System.out.println("Guess another letter or solve the whole thing: ");
+            String guess = sc.nextLine();
+            if ( guess.length() == 1 ) {
+                char letter = guess.charAt(0);
+                boolean found = false;
+                for (int i = 0; i < wordLength; i++) {
+                    if (word.charAt(i) == letter) {
+                        hiddenWord[i] = letter;
+                        found = true;
+                    }
+                }
+                if (!found) guesses++;
+                if (new String(hiddenWord).equals(word)) {
+                    System.out.println("\n\n\n\n\n\n\n\n\n\n\nCongratulations! You solved the word: " + word);
+                    break;
+                }
             } else {
-                System.out.println("A-");
+                if (guess.equals(word)) {
+                    System.out.println("\n\n\n\n\n\n\n\n\n\n\nCongratulations! You solved the word: " + word);
+                    break;
+                } else guesses++;
             }
-
-        } else if (userNum >= 80) {
-
-            if (userNum > 84) {
-                System.out.println("B+");
-            } else {
-                System.out.println("B-");
-            }
-
-        } else if ( userNum >= 67 ){
-
-            if (userNum > 74) {
-                System.out.println("C+");
-            } else {
-                System.out.println("C-");
-            }
-
-        } else if ( userNum >= 60 ){
-
-            if (userNum > 64) {
-                System.out.println("D+");
-            } else {
-                System.out.println("D-");
-            }
-
-        } else {
-            System.out.println('F');
+        }
+        if (guesses == maxGuesses) {
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\nYou have used up all of your guesses. The word was: " + word);
         }
     }
 }

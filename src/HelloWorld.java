84f8f4b25a2b46d08b785d7cb9718c363d4e3895
Leonard Karoly <leonard.a.karoly@gmail.com>
2023-02-13 12:10:59 -0600

    Hello World

---
+++ src/HelloWorld.java
@@ -0,0 +1,29 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 22;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "this is my string";
+        System.out.println(myString);
+
+        float myNumber = 3.14F;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+    }
+
+}

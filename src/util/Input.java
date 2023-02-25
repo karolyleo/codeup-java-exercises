2afac994c3d59c35c8adf779dbdb4c8ae5e8e45b
Leonard Karoly <leonard.a.karoly@gmail.com>
2023-02-25 00:01:13 -0600

    exception testing

--- src/util/Input.java
+++ src/util/Input.java
@@ -22,52 +22,112 @@ public class Input {
         int num;
         do {
             System.out.printf("Enter a number between %d and %d: ", min, max);
-            while (!this.scanner.hasNextInt()) {
+            String input = this.getString();
+            try {
+                num = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
                 System.out.println("Invalid input. Please enter a number.");
-                this.scanner.next();
+                num = min - 1; // set num to a value outside the min-max range to trigger the do-while loop
             }
-            num = this.scanner.nextInt();
         } while (num < min || num > max);
-        this.scanner.nextLine(); // consume the remaining newline character
-//        System.out.println(num);
         return num;
     }
+
     public int getInt() {
-        while (!this.scanner.hasNextInt()) {
-            System.out.println("Invalid input. Please enter a number.");
-            this.scanner.next();
-        }
-        int num = this.scanner.nextInt();
-        this.scanner.nextLine(); // consume the remaining newline character
-        System.out.println(num);
+        int num;
+        do {
+            String input = this.getString();
+            try {
+                num = Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number.");
+                num = Integer.MIN_VALUE; // set num to a very small value to trigger the do-while loop
+            }
+        } while (num == Integer.MIN_VALUE);
         return num;
     }
+
     public double getDouble(double min, double max) {
         double num;
         do {
             System.out.printf("Enter a decimal number between %f and %f: ", min, max);
-            while (!this.scanner.hasNextDouble()) {
+            String input = this.getString();
+            try {
+                num = Double.parseDouble(input);
+            } catch (NumberFormatException e) {
                 System.out.println("Invalid input. Please enter a decimal number.");
-                this.scanner.next();
+                num = min - 1; // set num to a value outside the min-max range to trigger the do-while loop
             }
-            num = this.scanner.nextDouble();
         } while (num < min || num > max);
-        this.scanner.nextLine(); // consume the remaining newline character
-        System.out.println(num);
         return num;
     }
 
     public double getDouble() {
-        while (!this.scanner.hasNextDouble()) {
-            System.out.println("Invalid input. Please enter a decimal number.");
-            this.scanner.next();
-        }
-        double num = this.scanner.nextDouble();
-        this.scanner.nextLine(); // consume the remaining newline character
-//        System.out.println(num);
+        double num;
+        do {
+            String input = this.getString();
+            try {
+                num = Double.parseDouble(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a decimal number.");
+                num = Double.MIN_VALUE; // set num to a very small value to trigger the do-while loop
+            }
+        } while (num == Double.MIN_VALUE);
         return num;
     }
+
 }
+/**
+ *  public int getInt(int min, int max) {
+ *         int num;
+ *         do {
+ *             System.out.printf("Enter a number between %d and %d: ", min, max);
+ *             while (!this.scanner.hasNextInt()) {
+ *                 System.out.println("Invalid input. Please enter a number.");
+ *                 this.scanner.next();
+ *             }
+ *             num = this.scanner.nextInt();
+ *         } while (num < min || num > max);
+ *         this.scanner.nextLine(); // consume the remaining newline character
+ * //        System.out.println(num);
+ *         return num;
+ *     }
+ *     public int getInt() {
+ *         while (!this.scanner.hasNextInt()) {
+ *             System.out.println("Invalid input. Please enter a number.");
+ *             this.scanner.next();
+ *         }
+ *         int num = this.scanner.nextInt();
+ *         this.scanner.nextLine(); // consume the remaining newline character
+ *         System.out.println(num);
+ *         return num;
+ *     }
+ *     public double getDouble(double min, double max) {
+ *         double num;
+ *         do {
+ *             System.out.printf("Enter a decimal number between %f and %f: ", min, max);
+ *             while (!this.scanner.hasNextDouble()) {
+ *                 System.out.println("Invalid input. Please enter a decimal number.");
+ *                 this.scanner.next();
+ *             }
+ *             num = this.scanner.nextDouble();
+ *         } while (num < min || num > max);
+ *         this.scanner.nextLine(); // consume the remaining newline character
+ *         System.out.println(num);
+ *         return num;
+ *     }
+ *
+ *     public double getDouble() {
+ *         while (!this.scanner.hasNextDouble()) {
+ *             System.out.println("Invalid input. Please enter a decimal number.");
+ *             this.scanner.next();
+ *         }
+ *         double num = this.scanner.nextDouble();
+ *         this.scanner.nextLine(); // consume the remaining newline character
+ * //        System.out.println(num);
+ *         return num;
+ *     }
+ */
 
 /**Create a package inside of src named util. Inside of util, create a class named Input that has a private field named scanner.
  *  When an instance of this object is created, the scanner field should be set to a new instance of the Scanner class. The class should have the following methods, all of which return command line input from the user:
